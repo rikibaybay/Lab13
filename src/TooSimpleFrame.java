b5479ac47c309c7ed792e329093d30337baffbb9
rikibaybay <kolyaokon00.06.18@gmail.com>
2022-04-15 14:19:39 +0900

    Initial commit

---
+++ src/TooSimpleFrame.java
@@ -0,0 +1,11 @@
+package com.company;
+
+import java.awt.*;
+
+public class TooSimpleFrame extends Frame{
+    public static void main(String[] args) {
+        Frame fr = new TooSimpleFrame();
+        fr.setSize(400, 150);
+        fr.setVisible(true);
+    }
+}
